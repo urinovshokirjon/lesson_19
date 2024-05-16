73cae96ed6543a99c64135b0db72c6071bf36557
urinovshokirjon <urinovshokirjon93@gmail.com>
2024-05-16 21:20:21 +0500

    commit Car

--- src/main/java/uz/urinov/lesson_19/Car.java
+++ src/main/java/uz/urinov/lesson_19/Car.java
@@ -1,4 +1,32 @@
 package uz.urinov.lesson_19;
 
 public class Car {
+    private String make;
+    private String model;
+    private int year;
+
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
